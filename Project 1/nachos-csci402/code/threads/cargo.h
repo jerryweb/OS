14cc9645a26eb768ee23a862174aff913046a8c0
Jerry Webb <jerryweb@usc.edu>
2015-06-09 00:31:04 -0700

    fixed cargo issue

--- Project 1/nachos-csci402/code/threads/cargo.h
+++ Project 1/nachos-csci402/code/threads/cargo.h
@@ -1,28 +1,22 @@
-// Check-in staff class. Takes luggage from the global conveyor, puts
-//  it in its proper airplane, and records statistics for the manager.
+//This is the check-in staff class 
 
 #ifndef CARGO_H
 #define CARGO_H
-
+#endif
 #include "copyright.h"
 #include "thread.h"
 #include "list.h"
-#include "airport.h"
+#include "airportobjects.h"
 
-class Cargo
-{
+class Cargo : public Thread {
 	public:
 		Cargo(int id, Airport* airport);
 		~Cargo();
-		void Run();
+		void doStuff();
 
 	private: // TODO: change from hard-coded numbers.
 		int id;             // ID of the cargo handler. Unique.
-        Airport* airport;   // "Airport"" construct, containing all
-                            //  public data.
-        int* luggage;       // Records how many bags this individual
-                            //  staff has processed, split by airline.
-        int* weight;        // Records the total weight of bags this
-                            //  individual staff has processed, split
-                            //  by airline.
+        Airport* airport;   // "Airport"" construct, containing all public data.
+        int luggage[3];     // Records how many bags this individual staff has processed, split by airline.
+        int weight[3];      // Records the total weight of bags this individual staff has processed, split by airline.
 }
