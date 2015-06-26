bb44ec932453d2f58f4c455869d13c1068cee9a7
unknown <lee533@usc.edu>
2015-06-25 18:40:56 -0700

    start liaison conversion

--- Project 2/nachos-csci402/code/test/airportStructs.h
+++ Project 2/nachos-csci402/code/test/airportStructs.h
@@ -1,42 +1,58 @@
 /*This is the struct for all of the public airport data*/
 
-typedef enum  {true, false} bool;
+typedef enum  {false, true} bool;
 typedef enum  {L_FREE, L_BUSY, L_NONE} LiaisonState;
-typedef enum  {CI_FREE, CI_BUSY, CI_BREAK, CI_CLOSED, CI_NONE}CheckinState;
-typedef enum  {C_BUSY, C_BREAK, C_NONE}CargoState;
-typedef enum  {BOARDING, BOARDED}AirlineState;
-typedef enum  {SO_BUSY, SO_FREE}ScreenState;
-typedef enum  {SC_BUSY, SC_FREE}SecurityState;
+typedef enum  {CI_FREE, CI_BUSY, CI_BREAK, CI_CLOSED} CheckinState;
+typedef enum  {C_BUSY, C_BREAK} CargoState;
+typedef enum  {SO_BUSY, SO_FREE} ScreenState;
+typedef enum  {SC_BUSY, SC_FREE} SecurityState;
 
 /*This is the struct that holds all of the public data for the airport*/
 typedef struct 
 {
 	int numAirlines; 
 	struct Passenger* passengerArray[20];
-}Airport;
+    struct Liaison* liaisonArray[5];
+    struct Passenger* liaisonQueues[5];
+    int liaisonManagerLock;
+    int liaisonManagerCV;
+    int liaisonLineLock;
+    int liaisonLineCV[5];
+    int liaisonLock[5];
+    int liaisonCV[5];
+    LiaisonState liaisonState[5];
+} Airport;
 
 typedef struct           
 {
 	int airlineCode;     
 	int weight;          
-}Luggage;
+} Luggage;
 
 typedef struct             
 {
 	int airline;         
 	bool executive;                          
-}Ticket;
+} Ticket;
 
 typedef struct       
 {
 	int gate;            
 	int seatNum;         
-}BoardingPass;
+} BoardingPass;
 
 typedef struct {
 	int id;
 	int airline;
-	Ticket ticket;
 	Airport airport;
+	Ticket ticket;
 	BoardingPass boardingPass;
-}Passenger;
+} Passenger;
+
+typedef struct {
+	int id;
+	Airport airport;
+	int passengers[3];
+    int luggageCount[3];
+    int luggageWeight[3];
+} Liaison;
