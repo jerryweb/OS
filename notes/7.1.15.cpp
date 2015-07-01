5893029f5c463a93cde6e4342101dbe679682e38
Jerry Webb <jerryweb@usc.edu>
2015-07-01 12:57:54 -0700

    Adding Notes
    July 1st, 2015

--- notes/7.1.15.cpp
+++ notes/7.1.15.cpp
@@ -1,5 +1,6 @@
 Project 3 -3 parts
-	parts 1 & 2 - Demand Page virtual memory 	//dont preload anything (no bitmap finds in the constructor; also we can no longer do a new page table each time)
+	parts 1 & 2 - Demand Page virtual memory 	//dont preload anything (no bitmap finds in the constructor; also we 
+												//can no longer do a new page table each time)
 	part 3 - networking - client server			//should work even if parts 1 & 2 are not
 		distributed locks/CVs
 
@@ -115,7 +116,7 @@ Project 3 -3 parts
 
 			Similar Changes in Fork IF you are making a new pageTable each time
 
-			On a PFE from step 2		//This is from step 2
+			On a PFE from step 2				//This is from step 2
 				int ppn = 1;
 				for (i =0 to numPhysPages){
 					if(/*check 3 values */){
@@ -131,3 +132,119 @@ Project 3 -3 parts
 
 
 				Populate the TLB from IPT 		//also from step 2
+
+			Handling an IPT Miss
+				- allocate 1 memory pageTable
+				- Copy from executable (if needed);
+				- Update page table 
+					* valid bit = true 
+					* physicalPage 
+				- populate IPT from page table
+
+			Issue 1: We do not have header info!
+				  2: delete executable;			//will need to comment this out
+				  								/*File I/O is the slowest thign in an operating system; so we
+				  								want to avoid it*/
+
+			#1: Must keep executable open until process is done
+				- move executable variable into AddrSpace
+				- comment out the delete executable
+
+			#2: noffH.code.inFileAddr + (i * PageSize)	
+				Solution: Add data to page table 
+				Need another new CLass 			//inheritance is the best way 
+					- byte offset				//take the thrid arguement and store it in the field 
+					- in executable, or not, or swapfile	//can use an int or booleans /*1 one means swap, 2 means neither*/
+			_____________
+			|Code 		|
+			|-----------|
+			|I.data		|
+			|-----------|
+			|-----------|	int inExec = divRoundUp(code & initData);
+			|U.data		|	
+			|			|
+			|			|
+			|___________|
+
+				//in AddrSpace Constructor
+				for(){
+					set not inExec				//as a default, you can set everything to not be in the executable 
+					if(i < inExec){
+						set byte offset
+						set in executable
+					}
+				}
+
+		Step 4 - D.P.V.M.
+			Set numPhysPages to 32 				//do not try and run your airport at this point; nachos will page fault too
+												//fast and too often
+				* memory will fill up 
+			On a P.F.E., on an IPT miss, bitMap->Find(); will return -1 	//this means there is nothing left
+			You select a page to replace 		//need a page replacement policy 
+				* 2 polices 
+				-PRAND	/*add this test to be able to run from command line*/
+					- random
+
+				-PFIFO	/*add this test to be able to run from command line*/
+					- FIFO - must use a queue
+						anytime on a Find(not -1); 
+							append ppn to queue
+						on Find of -1, take from entry
+						@ end of IPTMiss - add to queue
+
+
+			Selected page may be dirty 			//means page may have been modified
+			- must be saved to swapfile
+			Only 1 swapfile for entire O.S.
+				-system.h/.cc 					//stays open for the entire process once opened
+				-open in Initialize 
+
+			On IPTMiss:
+				int ppn = bitMap->Find();
+				if(ppn == -1){
+					ppn = handleMemoryFull(		);
+				}
+				//Rest of step 
+
+
+			matmult -> value to exit 7220
+			//if you get a value between 0 and 7220, it is most likely an issue with the swapfile
+			//try making the numPhysPages big and see if it actually works. If it does, then you know that the 1st three steps may
+			//be correct 
+			sort 	-> value to exit 1023		//sort can take much longer than matmult
+			//You should just print the value sent to exit when running these tests to make sure they work; start without -rs
+
+			nachos -x ../test/matmult
+
+			Final Test:							//these are 2 seperate tests
+				fork 2 matmult/sort
+				exec 2 matmult/sort
+
+			How to handle swapfile
+			 Cache of dirty, evicted memory pages 
+			 Create a swapfile bitMap - 5000	//check for -1; if it is -1, just print out an error message 
+			 	outside of nachos
+			 	* create swapfile & open
+			 	//make sure that the name of the swapfile already exists in the directory... can just make something up in notepad???
+
+			Dirty Bits
+			 Nachos only sets TLB dirty bits
+			 	anytime you change TLB - propagate dirty bit to TLB
+			 							if valid is true 
+			 	disable interrupts
+			 	if(machine->TLB[currentTLB].valid){
+			 		//copy dirty bit to IPT
+			 		machine->TLB[currentTLB]->_______=IPT[ppn]._______;
+			 	}
+
+			 	currentTLB = _____
+
+			 	R.I.
+
+Start on parts 1 and 2 over the weekend, and get this working.
+for networking you can use:
+on X11, you can use xterm& 
+nachos -m 0 -o 1 //the -m is the machine id 0 and it will send and listen for machine 1 
+You need to type://These are on the 2 seperate shells 
+	nachos -m 0 -o1
+	nachos -m 1 -o0
