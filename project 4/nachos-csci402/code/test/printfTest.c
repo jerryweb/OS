9a3e160f98424ed1c8c0ba13d49b23bf2a9cba1d
unknown <lee533@usc.edu>
2015-07-25 18:05:34 -0700

    quick test

--- project 4/nachos-csci402/code/test/printfTest.c
+++ project 4/nachos-csci402/code/test/printfTest.c
@@ -9,11 +9,11 @@ int b, c;
 int
 main()
 {
-    /* Invalid strings  - should print 1 error */
+    /* Invalid strings  - should print 1 error 
     Printf((char*)10, 10, 0, 0);
-    /* Invalid length - should not print anything */
+    /* Invalid length - should not print anything 
     Printf("Hi\n", -1, 0, 0);
-    /* Invalid number of parameters - should print 2 errors */
+    /* Invalid number of parameters - should print 2 errors 
     Printf("Hi\n", 3, -1, 0);
     Printf("Hi\n", 3, 10, 0);
     /* Correct calls:
@@ -22,10 +22,13 @@ main()
         Hi 1 2
         Hi 10 11 12
         Hi 23 45 67 89
-    */
+    
     Printf("Hi\n", 3, 0, 0);
     Printf("Hi %d\n", 6, 1, 101);
     Printf("Hi %d %d\n", 9, 2, 102);
     Printf("Hi %d %d %d\n", 12, 3, 101112);
     Printf("Hi %d %d %d %d\n", 15, 4, 123456789);
+    */
+    Printf("Hi\n", 3, 0, 0);
+    Exec("../test/printfTest", 18);
 }
